7a62c81d1db543ceceb4eda3e02861b32e213189
Benguerrero1 <benjaminguerrero1@gmail.com>
2015-11-24 11:38:44 -0300

    Se corrigen bugs en la ubicación de los imports. El programa retorna H_0 con éxito.

--- hubble.py
+++ hubble.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
-"""
-Este codigo extrae los datos medidos de Hubble y deriva la constante de Hubble
-incluyendo su intervalo de confianza al 95%
-"""
-from scipy.optimize import (leastsq, curve_fit)
 from __future__ import division
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import stats
+from scipy.optimize import (leastsq, curve_fit)
+"""
+Este codigo extrae los datos medidos de Hubble y deriva la constante de Hubble
+incluyendo su intervalo de confianza al 95%, usando curve_fit.
+"""
 
 def f_hubble1(params, d):
     H = params
@@ -24,6 +24,8 @@ def funcion_a_minimizar1(H, d):
 def funcion_a_minimizar2(H, v):
     params = H
     return f_hubble2(params, v)
+    
+#Main
 
 hubble = np.loadtxt("data/hubble_original.dat")
 d = hubble[:, 0]
@@ -32,3 +34,5 @@ v = hubble[:, 1]
 a_optimo1, a_covarianza1 = curve_fit(funcion_a_minimizar1, d, v, 2)
 a_optimo2, a_covarianza2 = curve_fit(funcion_a_minimizar2, d, v, 2)
 a_promedio = (a_optimo1 + a_optimo2) / 2
+H_0 = a_promedio
+print ("H_0 = " + str(H_0))
