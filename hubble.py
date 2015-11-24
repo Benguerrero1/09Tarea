0ef042b4505cd943ee4890159954783943ba55e8
Benguerrero1 <benjaminguerrero1@gmail.com>
2015-11-24 11:21:23 -0300

    Se repara la funcion de extraccion de datos, y se definen las funciones a minimizar.

--- hubble.py
+++ hubble.py
@@ -3,10 +3,21 @@
 Este codigo extrae los datos medidos de Hubble y deriva la constante de Hubble
 incluyendo su intervalo de confianza al 95%
 """
-
+from __future__ import division
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import stats
 
-hubble = np.loadtxt("C:/Users/Administrador/09Tarea/data/hubble_original.dat")
-print hubble
+def f_hubble(params, d):
+    H = params
+    return H * d
+
+def funcion_a_aminimizar(H, d):
+    params = H
+    return f_hubble(params, d)
+
+hubble = np.loadtxt("data/hubble_original.dat")
+d = hubble[:, 0]
+v = hubble[:, 1]
+
+
