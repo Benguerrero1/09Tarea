23b411b481ab802e17e0da139e9c6d5ba411cd09
Benguerrero1 <benjaminguerrero1@gmail.com>
2015-11-24 23:47:44 -0300

    Se crean los gráficos para la P3, y se adjunta el informe. Tarea completa.

--- MC.py
+++ MC.py
@@ -36,7 +36,27 @@ def mc(banda_i, error_i, banda_z, error_z, c_0):
         muestra_i = banda_i + error_i * r
         muestra_z = banda_z + error_z * r
         pendiente[j], cte[j] = np.polyfit(muestra_i, muestra_z, 1)
+    
+    fig2 = plt.figure()
+    ax2 = fig2.add_subplot(111)
+    ax2.hist(pendiente, bins=30)
+    ax2.axvline(c[0], color='r', label="valor obtenido")
+    plt.legend(loc=2)
+    ax2.set_title("Simulacion de Montecarlo (pendientes)")
+    ax2.set_xlabel("pendiente [adimensional]")
+    ax2.set_ylabel("frecuencia")
+    plt.savefig("mc_1.jpg")
 
+    fig3 = plt.figure()
+    ax3 = fig3.add_subplot(111)
+    ax3.hist(cte, bins=30)
+    ax3.axvline(c[1], color='r', label="valor obtenido")
+    plt.legend(loc=2)
+    ax3.set_title("Simulacion de Montecarlo (coef de posicion)")
+    ax3.set_xlabel("coef de posicion [adimensional]")
+    ax3.set_ylabel("frecuencia")
+    plt.savefig("mc_2.jpg")
+    
     pendiente = np.sort(pendiente)
     cte = np.sort(cte)
     limite_bajo_1 = pendiente[int(Nmc * 0.025)]
@@ -52,11 +72,11 @@ def mc(banda_i, error_i, banda_z, error_z, c_0):
 
 #Main
 
-data = np.loadtxt("data/DR9Q.dat", usecols=(80, 81, 82, 83))
-banda_i = data[:, 0] * 3.631
-error_i = data[:, 1] * 3.631
-banda_z = data[:, 2] * 3.631
-error_z = data[:, 3] * 3.631
+datos = np.loadtxt("data/DR9Q.dat", usecols=(80, 81, 82, 83))
+banda_i = datos[:, 0] * 3.631
+error_i = datos[:, 1] * 3.631
+banda_z = datos[:, 2] * 3.631
+error_z = datos[:, 3] * 3.631
 c = np.polyfit(banda_i, banda_z, 1)
 print("recta : {}x + {}".format(c[0], c[1]))
 intervalo_confianza = mc(banda_i, error_i, banda_z, error_z, c)
